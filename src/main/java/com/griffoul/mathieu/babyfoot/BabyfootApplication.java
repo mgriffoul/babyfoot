7cbe9d3a7a5daf41b0ebc885173ecccec145b0fd
mathieu_griffoul <mgriffoul@oui.sncf>
2019-01-08 19:41:38 +0100

    Initial commit

---
+++ src/main/java/com/griffoul/mathieu/babyfoot/BabyfootApplication.java
@@ -0,0 +1,14 @@
+package com.griffoul.mathieu.babyfoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyfootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyfootApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/griffoul/mathieu/babyfoot/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.griffoul.mathieu.babyfoot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BabyfootApplication.class);
+	}
+
+}
+
